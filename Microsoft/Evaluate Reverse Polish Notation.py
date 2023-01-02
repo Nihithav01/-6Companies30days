9814b0f9ced7c6449d45233a2ca0278396626183
Nihitha Vemulapalli <87615583+Nihithav01@users.noreply.github.com>
2023-01-02 19:00:29 +0530

    Update Evaluate Reverse Polish Notation.py

--- Microsoft/Evaluate Reverse Polish Notation.py
+++ Microsoft/Evaluate Reverse Polish Notation.py
@@ -1,1 +1,27 @@
 
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+            stack = []
+           
+            
+            for token in tokens:
+                if token == "+" or token == "*" or token  == "-" or token == "/":
+                    a = stack.pop()
+                    b = stack.pop()
+                    
+                    if token == "+":
+                        res  =  a + b
+                    elif token == "-":
+                        res = b - a
+                    elif token == "*":
+                        res = a * b
+                    elif token == "/":
+                        if abs(b) < abs(a):
+                            res = 0
+                        else:
+                            res = b / a
+                    
+                    stack.append(int(res)) 
+                else:
+                    stack.append(int(token))
+            return stack.pop()
